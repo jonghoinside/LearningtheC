c09c6d3a91c67e737affbad08ff2298ef3e0ab37
jonghoinside <jonghoinside@gmail.com>
2021-06-02 21:36:33 +0900

    Code 수정

--- [C] Practices/01_Baseball game/baseball.c
+++ [C] Practices/01_Baseball game/baseball.c
@@ -11,49 +11,46 @@ int main(void)
 	int ball;
 	
 	int count = 0;
-	
 // Question
+    printf("Nice to meet you. Welcome to the world of An Jong Woo.\n");
+    while(1) {
+        printf("\nLet's start the game now.\n");
+        strike = 0;
+        ball = 0;
+        srand(time(NULL));
 
-	srand(time(NULL));
-/*
-	while (question[0] == question[1] || 
-			question[1] == question[2] || 
-			question[0] == question[2]) {
-		question[0] = rand() % 9 + 1;
-		question[1] = rand() % 9 + 1;
-		question[2] = rand() % 9 + 1;
-	}
-*/	
-	question[0] = rand() % 9 + 1;
-	do question[1] = rand() % 9 + 1;
-	while (question[0] == question[1]);
-	
-	do question[2] = rand() % 9 + 1;
-	while (question[0] == question[2] || question[1] == question[2]);
-	
-	printf("question : %d %d %d\n", question[0], question[1], question[2]);
-	
-	while (strike != 3) {
-		strike = 0;
-		ball = 0;
-		
-		scanf("%d %d %d", &answer[0], &answer[1], &answer[2]);
-		
-		for (int i = 0; i < 3; ++i)		
-			for (int j = 0; j < 3; ++j)
-				if (question[i] == answer[j]) {
-					if (i == j)
-						++strike;
-					else
-						++ball;
-				}
-		
-		if (strike != 3)
-			printf("#strike : %d\t#ball : %d\n\n", strike, ball);
-		
-		++count;
-	} 
-	
-	printf("Congraturation! count : %d\n", count);
-	return 0;
+        question[0] = rand() % 9 + 1;
+        do question[1] = rand() % 9 + 1;
+        while (question[0] == question[1]);
+
+        do question[2] = rand() % 9 + 1;
+        while (question[0] == question[2] || question[1] == question[2]);
+
+//        printf("question : %d %d %d\n", question[0], question[1], question[2]);
+
+        while (strike != 3) {
+            strike = 0;
+            ball = 0;
+
+            printf("Please input number : ");
+            scanf("%d %d %d", &answer[0], &answer[1], &answer[2]);
+
+            for (int i = 0; i < 3; ++i)
+                for (int j = 0; j < 3; ++j)
+                    if (question[i] == answer[j]) {
+                        if (i == j)
+                            ++strike;
+                        else
+                            ++ball;
+                    }
+
+            if (strike != 3)
+                printf("#strike : %d\t#ball : %d\n\n", strike, ball);
+
+            ++count;
+        }
+
+        printf("Congraturation! count : %d\n", count);
+    }
+    return 0;
 }
